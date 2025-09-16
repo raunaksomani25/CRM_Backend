e9bea4bd0d0f579bd4f0c5430e025330a00f7a43
raunaksomani25 <156603817+raunaksomani25@users.noreply.github.com>
2025-09-16 09:32:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,8 +25,14 @@ app.use(
     secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production", 
+      httpOnly: true,
+      sameSite: "none", 
+    },
   })
 );
+
 app.use(cors({
   origin: "https://crm-frontend-theta-six.vercel.app", 
   credentials: true               
@@ -50,6 +56,7 @@ app.use("/api/ai", require("./src/routes/ai"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
